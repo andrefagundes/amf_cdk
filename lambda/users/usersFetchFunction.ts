adb51f77b87d7d218633f88ce051da8a46b1fd29
andrefagundesmkt <andrefagundesmkt@gmail.com>
2023-10-03 12:12:51 -0300

    feat: Criação de api gateway users e lambdas.

---
+++ lambda/users/usersFetchFunction.ts
@@ -0,0 +1,28 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const lambdaRequestId = context.awsRequestId;
+    const apiRequestId = event.requestContext.requestId;
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+
+    if (event.resource === '/users') {
+        if (event.httpMethod === 'GET') {
+            console.log('REQUISITION GET');
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'GET PRODUCTS OK'
+                })
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: 'GET PRODUCTS ERROR'
+        })
+    }
+}
