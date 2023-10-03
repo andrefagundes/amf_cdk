ab3da6d9a8b6efd90ced205edaffb4967c3a7621
andrefagundesmkt <andrefagundesmkt@gmail.com>
2023-10-03 18:28:00 -0300

    style: padronizando código

--- lib/usersApi-stack.ts
+++ lib/usersApi-stack.ts
@@ -1,38 +1,41 @@
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
-import * as apiGateway from "aws-cdk-lib/aws-apigateway"
-import * as cwLogs from "aws-cdk-lib/aws-logs"
-import * as cdk from "aws-cdk-lib"
-import { Construct } from "constructs"
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as cwLogs from 'aws-cdk-lib/aws-logs';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 interface UsersApiStackProps extends cdk.StackProps {
-    usersFetchHandler: lambdaNodeJS.NodejsFunction
+  usersFetchHandler: lambdaNodeJS.NodejsFunction;
 }
 
 export class UsersApiStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: UsersApiStackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: UsersApiStackProps) {
+    super(scope, id, props);
 
-        const logGroup = new cwLogs.LogGroup(this, "UsersApiLogs");
-        const api = new apiGateway.RestApi(this, "UsersApi", {
-            restApiName: "UsersApi",
-            deployOptions: {
-                accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
-                accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
-                    httpMethod: true,
-                    ip: true,
-                    protocol: true,
-                    requestTime: true,
-                    resourcePath: true,
-                    responseLength: true,
-                    status: true,
-                    caller: true,
-                    user: true
-                })
-            }
-        });
+    const logGroup = new cwLogs.LogGroup(this, 'UsersApiLogs');
+    const api = new apiGateway.RestApi(this, 'UsersApi', {
+      restApiName: 'UsersApi',
+      deployOptions: {
+        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
+    });
 
-        const usersFetchIntegration = new apiGateway.LambdaIntegration(props.usersFetchHandler);
-        const usersResource = api.root.addResource("users");
-        usersResource.addMethod("GET", usersFetchIntegration);
-    }
+    const usersFetchIntegration = new apiGateway.LambdaIntegration(
+      props.usersFetchHandler,
+    );
+
+    const usersResource = api.root.addResource('users');
+    usersResource.addMethod('GET', usersFetchIntegration);
+  }
 }
--- lib/usersApp-stack.ts
+++ lib/usersApp-stack.ts
@@ -1,25 +1,29 @@
-import * as lambda from "aws-cdk-lib/aws-lambda"
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
-import * as cdk from "aws-cdk-lib"
-import { Construct } from "constructs"
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class UsersAppStack extends cdk.Stack {
-    readonly usersFetchHandler: lambdaNodeJS.NodejsFunction;
+  readonly usersFetchHandler: lambdaNodeJS.NodejsFunction;
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        this.usersFetchHandler = new lambdaNodeJS.NodejsFunction(this, "UsersFetchFunction", {
-            runtime: lambda.Runtime.NODEJS_16_X,
-            functionName: "UsersFetchFunction",
-            entry: "lambda/users/usersFetchFunction.ts",
-            handler: "handler",
-            memorySize: 128,
-            timeout: cdk.Duration.seconds(5),
-            bundling: {
-                minify: true,
-                sourceMap: false,
-            }
-        })
-    }
+    this.usersFetchHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      'UsersFetchFunction',
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        functionName: 'UsersFetchFunction',
+        entry: 'lambda/users/usersFetchFunction.ts',
+        handler: 'handler',
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+      },
+    );
+  }
 }
